63dfc7d4bcac5bf5d65b2441b0f7aea22882bb3f
Andre Xavier <lordlagon@gmail.com>
2019-09-25 02:29:17 -0300

    Add project files.

---
+++ Pontofopag/Views/PoliticaPrivacidadePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Pontofopag.Views
+{
+    public partial class PoliticaPrivacidadePage : ContentPage
+    {
+        public PoliticaPrivacidadePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
